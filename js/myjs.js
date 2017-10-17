7f3ae2741e5383206dd60379885c2b5981979c61
soocastle <solange.tami@gmail.com>
2017-10-17 17:05:11 +0200

    Last edit day two
    Fusion julie et solange

---
+++ js/myjs.js
@@ -0,0 +1,14 @@
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+
+// $(document).ready(function(){
+// 	$(".facet-title").click() {
+// 		$(".element").toggle("slow");
+// 	};
+// })
