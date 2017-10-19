04f3bc4bb1fc8da29e23c27f2a1c81c3abeb0c12
lili-r <ramierjulie@gmail.com>
2017-10-19 17:05:09 +0200

    Mise à jour/jeudi
    Fusion des dossiers solange + julie

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,24 @@
+
+
+// $(document).ready(function(){
+// 	$(".facet-title").click() {
+// 		$(".element").toggle("slow");
+// 	};
+// })
+$(document).ready(function(){
+	$(".facet-title").click(function() {
+		console.log('hehe');
+		$(".element").toggle("slow");
+	});
+
+
+	$(".categorie-1").click(function () {
+    	$(".categorie-1").addClass("active");
+	});
+
+
+
+
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
@@ -7,8 +28,55 @@ $(document).on('click', 'a[href^="#"]', function (event) {
 });
 
 
-// $(document).ready(function(){
-// 	$(".facet-title").click() {
-// 		$(".element").toggle("slow");
-// 	};
-// })
+$.getJSON("https://api.airtable.com/v0/appIiozFdX3Mv6NcD/musics?api_key=keythl8GoS7nMwIzL", 
+		function(data) {
+
+		counter = 1;
+		$.each(data.records ,function( index, value ) {
+
+			$("#musiques").append("\
+						<div class='col-md-6 col-sm-6 col-xs-12'>\
+						<ul>\
+								<li class='first'> <p>"+counter+"</p> \
+									<div class='trais-hone'>\
+									</div>\
+									<iframe src='"  + value.fields['spotify-link'] + "' frameborder='0' allowtransparency='true'></iframe>\
+								</li>\
+									<input type='checkbox' name='' id='btn' />\
+								<label class='btn-love' for='btn'></label>\
+									<div class='vote'>\
+										<img src='img/plusone.png'>\
+									</div>\
+							</ul>\
+						</div>");
+
+			counter = counter +1;
+
+			 // $('body').append("<div class='row'>" + "<div class='col-md-6 col-sm-6 col-xs-12'>" + "<ul>" + "<li>" +"<p>3</p>" + "<div class='trais-h'>" 
+			 // 	+ "</div>" + " <iframe src=' "  + value.fields['spotify-link'] + " 'frameborder='0' allowtransparency='true'></iframe>" 
+			 // 	+ "<div class='like'><img src='img/like.png'></div>" + "</div class='vote'><img src='img/plusone.png'>" + "</div>" + "</ul>" + "</div>"	+
+
+
+			 // 	"<div class='col-md-6 col-sm-6 col-xs-12'>" + "<ul>" + "<li>" +"<p>3</p>" + "<div class='trais-h'>" 
+			 // 	+ "</div>" + " <iframe src=' "  + value.fields['spotify-link'] + " 'frameborder='0' allowtransparency='true'></iframe>" 
+			 // 	+ "<div class='like'><img src='img/like.png'></div>" + "</div class='vote'><img src='img/plusone.png'>" + "</div>" + "</ul>" + "</div>"	+ "</div>"
+
+
+
+
+			 			 
+
+			 
+
+		});
+
+
+
+		//	console.log(data.records[0].fields.title);
+
+
+		}
+);
+
+});
+
